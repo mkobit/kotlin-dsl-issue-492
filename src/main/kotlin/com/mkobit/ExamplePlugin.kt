efd844dcc7526ef28dbbf7144351cd313ad63d19
Mike Kobit <mkobit@gmail.com>
2017-10-27 15:42:48 -0500

    Add in code for example

---
+++ src/main/kotlin/com/mkobit/ExamplePlugin.kt
@@ -0,0 +1,10 @@
+package com.mkobit
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ExamplePlugin : Plugin<Project> {
+  override fun apply(project: Project) {
+    project.extensions.create("exampleExtension", ExampleExtension::class.java, "hello")
+  }
+}
---
+++ src/test/kotlin/com/mkobit/ExampleTestKitTest.kt
@@ -0,0 +1,68 @@
+package com.mkobit
+
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Before
+import org.junit.Test
+import java.io.File
+
+class ExampleTestKitTest {
+
+  private lateinit var gradleKts: File
+  private lateinit var gradleRunner: GradleRunner
+
+  @Before
+  fun setUp() {
+    val projectDir = createTempDir().apply {
+      deleteOnExit()
+    }
+    gradleKts = projectDir.resolve("build.gradle.kts")
+    gradleKts.writeText("""
+      plugins {
+        id("com.mkobit.example-plugin")
+      }
+    """.trimIndent())
+    gradleRunner = GradleRunner.create()
+        .withProjectDir(projectDir)
+        .withPluginClasspath()
+  }
+
+
+  @Test
+  fun `can print out extensions`() {
+    gradleKts.appendText("""
+      println(extensions)
+    """.trimIndent())
+
+    gradleRunner.build()
+  }
+
+  @Test
+  fun `can access extension by name`() {
+    gradleKts.appendText("""
+      println(exampleExtension)
+    """.trimIndent())
+
+    gradleRunner.build()
+  }
+
+  @Test
+  fun `can access extension property`() {
+    gradleKts.appendText("""
+      println(exampleExtension.myValue)
+    """.trimIndent())
+
+    gradleRunner.build()
+  }
+
+  @Test
+  fun `can set extension property`() {
+    gradleKts.appendText("""
+      exampleExtension {
+        myValue = "hello there!"
+      }
+      println(exampleExtension.myValue)
+    """.trimIndent())
+
+    gradleRunner.build()
+  }
+}
